fc6de32964ecef66167359e7dd38eb5fca7a6ff2
Rüzgar <ruzgargurgen@gmail.com>
2022-06-20 22:13:34 +0300

    changed

--- spring-security/src/main/java/com/rgurgen/springsecurity/model/Role.java
+++
@@ -1,8 +0,0 @@
-package com.rgurgen.springsecurity.model;
-
-public enum Role {
-	USER, // default
-	ADMIN, // admin manager, CRUD
-	SYSTEM_MANAGER // internal operations.
-
-}
--- spring-security/src/main/java/com/rgurgen/springsecurity/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package com.rgurgen.springsecurity.service;
-
-import com.rgurgen.springsecurity.model.User;
-
-public interface AuthenticationService {
-	 User signInAndReturnJWT(User signInRequest);
-
-}
